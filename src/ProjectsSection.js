98f7b56366d45eed543f5fe56a0d30be63de03c6
YuanSamuel <samuel6161@gmail.com>
2021-02-04 21:48:37 -0600

    Projects responsive design

--- src/ProjectsSection.js
+++ src/ProjectsSection.js
@@ -5,8 +5,11 @@ import ProjectPhoto3 from "./assets/images/ProjectPhoto3.jpg";
 import GitHubLogo from "./assets/GitHubLogo.png";
 import LaunchIcon from "./assets/LaunchIcon.png";
 import Button from "@material-ui/core/Button";
+import { useMediaQuery } from "react-responsive";
 
 export default function Projects() {
+  const isNarrow = useMediaQuery({ query: "(max-width: 900px" });
+
   function getTechList(tech) {
     return tech.map((name, index) => (
       <p key={name + index} className="tech-name">
@@ -15,10 +18,15 @@ export default function Projects() {
     ));
   }
 
+  var projectClass = isNarrow ? "project-portrait" : "project-landscape";
+  var linksClass = isNarrow
+    ? "project-links-portrait"
+    : "project-links-landscape";
+
   return (
     <div className="projects-list">
       <h1>Projects</h1>
-      <div className="project">
+      <div className={projectClass}>
         <img src={ProjectPhoto1} className="project-photo" />
         <div className="project-info">
           <h2 className="project-title">Personal Portfolio</h2>
@@ -29,19 +37,19 @@ export default function Projects() {
           <ProjectTechnologies>
             {getTechList(["Jekyll", "HTML", "SCSS", "Static Site"])}
           </ProjectTechnologies>
-        </div>
-        <div className="project-links">
-          <ProjectLink
-            type="GitHub"
-            link="https://github.com/MLH-Fellowship/portfolio-template"
-          />
-          <ProjectLink
-            type="Launch"
-            link="https://mlh-fellowship.github.io/portfolio-template"
-          />
+          <div className="project-links">
+            <ProjectLink
+              type="GitHub"
+              link="https://github.com/MLH-Fellowship/portfolio-template"
+            />
+            <ProjectLink
+              type="Launch"
+              link="https://mlh-fellowship.github.io/portfolio-template"
+            />
+          </div>
         </div>
       </div>
-      <div className="project">
+      <div className={projectClass}>
         <img src={ProjectPhoto2} className="project-photo" />
         <div className="project-info">
           <h2 className="project-title">Sample Project</h2>
@@ -51,7 +59,7 @@ export default function Projects() {
           </p>
         </div>
       </div>
-      <div className="project">
+      <div className={projectClass}>
         <img src={ProjectPhoto3} className="project-photo" />
         <div className="project-info">
           <h2 className="project-title">Sample Project 2</h2>
